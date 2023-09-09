3da33c925d607eafdf9d7bf84c95238a325dc51d
pisuthd <pisuth.dae@gmail.com>
2023-09-09 11:47:34 +0900

    [client] New Header

--- packages/client/src/components/Header.js
+++ packages/client/src/components/Header.js
@@ -14,87 +14,67 @@ import {
 } from "@suiet/wallet-kit";
 import useLegato from '@/hooks/useLegato'
 
-const Header = ({ landing }) => {
+
+const Header = () => {
 
     const router = useRouter()
     const wallet = useWallet();
 
     const { correctedChain } = useLegato()
-   
-    const { pathname } = router
 
-    if (landing) {
-        return (
-            <div class="grid grid-cols-2 gap-3 px-2 mt-4 mb-4 ">
-                <div class="col-span-1 flex flex-col">
-                    <div class="text-3xl w-full text-white font-bold flex flex-row">
-                        <Image
-                            src="/logo.png"
-                            width={60}
-                            height={60}
-                            alt="Logo"
-                        />
-                        <div className='font-mono text-sm mt-auto mb-auto ml-3 tracking-widest'>
-                            Legato Finance
-                        </div> 
-                    </div>
-                </div>
-                <div class="col-span-1 flex flex-col text-right">
-                    <Link href="/stake">
-                        <button className="px-8 py-3  m-2 mx-auto text-sm flex flex-row border rounded mr-0">
-                            <ExternalLink size={18} className="mr-2" />
-                            Launch App
-                        </button>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+    const { pathname } = router
 
     return (
         <>
-            <div class="grid grid-cols-3 gap-3 px-2 m-4 ">
-                <div class="col-span-1 flex flex-col">
-                    <div class="text-3xl w-full text-white flex flex-row">
-                        <Link href="/">
+            <nav
+                class="flex items-center justify-between flex-wrap  shadow">
+                <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+                    <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
+                        <Link href="https://legato.finance">
                             <Image
-                                src="/logo.png"
-                                width={60}
-                                height={60}
+                                src="/legato-logo.png"
+                                width={150}
+                                height={36}
                                 alt="Logo"
                             />
-                        </Link> 
+                        </Link>
                     </div>
                 </div>
-                <div class="col-span-1 flex">
-                    <div className='mx-auto mt-auto mb-auto text-sm'>
-                        <div class="grid grid-cols-3 gap-10 px-2 m-4 ">
-                            <Link className={`hover:underline ${pathname.includes("/trade") && "underline"}`} href="/trade">
-                                Trade
-                            </Link>
-                            <Link className={`hover:underline ${pathname.includes("/stake") && "underline"}`} href="/stake">
+                <div class="menu w-full  flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+                    <div class="text-md font-bold text-blue-700 lg:flex-grow">
+                        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                            <Link className={`border-b-2 ${pathname === ("/") ? "text-gray-200 border-blue-700" : "border-transparent hover:text-gray-200 hover:border-blue-700"} mx-1.5 sm:mx-6`} href="/">
                                 Stake
                             </Link>
-                            <Link className={`hover:underline ${pathname.includes("/faucet") && "underline"}`} href="/faucet">
-                                Faucet
+                            <Link className={`border-b-2 ${pathname.includes("/trade") ? "text-gray-200 border-blue-700" : "border-transparent hover:text-gray-200 hover:border-blue-700"} mx-1.5 sm:mx-6`} href="/trade">
+                                Trade
+                            </Link>
+                            <Link className={`border-b-2 ${pathname.includes("/portfolio") ? "text-gray-200 border-blue-700" : "border-transparent hover:text-gray-200 hover:border-blue-700"} mx-1.5 sm:mx-6`} href="/portfolio">
+                                Portfolio
                             </Link>
                         </div>
                     </div>
+                    <div class="flex ">
+                        <ConnectButton>
+                            Connect Wallet
+                        </ConnectButton>
+                    </div>
                 </div>
-                <div class="col-span-1 flex flex-col text-right">
-                    <ConnectButton>
-                        Connect Wallet
-                    </ConnectButton>
-                </div>
-            </div>
-            {wallet && wallet.connected && !correctedChain && (
+            </nav>
+            {/* <div class="flex h-40 w-full flex-row items-center justify-center">
+  <button class="animate-border inline-block rounded-md bg-white bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 bg-[length:400%_400%] p-1">
+    <span class="block rounded-md bg-slate-900 px-5 py-3 font-bold text-white"> algochurn.com </span>
+  </button>
+</div> */}
+            {/* {wallet && wallet.connected && !correctedChain && (
                 <div class=" border border-gray-400 text-gray-100 px-4 py-3 ml-7 mr-7 rounded relative" role="alert">
                     <strong class="font-bold">Incorrect chain!</strong>{` `}
-                    <span class="block sm:inline">Support Testnet only</span> 
+                    <span class="block sm:inline">Support Testnet only</span>
                 </div>
-            )}
+            )} */}
         </>
     )
 }
+
 
 export default Header
--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -15,6 +15,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        border: 'border 4s ease infinite',
+      },
+      keyframes: {
+        border: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
     },
   },
   plugins: [],
