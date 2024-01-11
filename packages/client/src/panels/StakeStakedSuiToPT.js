825b377adaeb0662263e09faa7ce539f2f4e2005
pisuthd <pisuth.dae@gmail.com>
2024-01-11 11:46:44 +0900

    Client] corrects the modal size mislength

--- packages/client/src/panels/StakeStakedSuiToPT.js
+++ packages/client/src/panels/StakeStakedSuiToPT.js
@@ -298,7 +298,7 @@ const Modal = ({
     return (
         <div class="fixed inset-0 flex items-center justify-center  z-50">
             <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
-            <div class={`relative   bg-gray-800 p-6 w-full ml-5 mr-7 mb-[200px]  border  border-gray-700 text-white rounded-lg`}>
+            <div class={`relative  bg-gray-800 p-6 w-full mb-[200px] max-w-lg  border  border-gray-700 text-white rounded-lg`}>
                 <h5 class="text-xl font-bold mb-2"> All Staked SUI ({totalAssets})</h5>
                 <button class="absolute top-3 right-3 text-gray-500 hover:text-gray-400" onClick={() => dispatch({ modal: false })}>
                     <X />
