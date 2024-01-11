5e401aa54b37a31a8b70e6bbf33b9305ed9c2223
pisuthd <pisuth.dae@gmail.com>
2024-01-11 12:08:05 +0900

    [client] disabled when the vault has matured

--- packages/client/src/panels/StakeStakedSuiToPT.js
+++ packages/client/src/panels/StakeStakedSuiToPT.js
@@ -65,12 +65,18 @@ const StakeStakedSuiToPT = ({
             const diffEpoch = Number(vault.maturity_epoch) - Number(summary.epoch)
             const matureDate = new Date((new Date(Number(summary.epochStartTimestampMs))).valueOf() + (Number(summary.epochDurationMs) * (diffEpoch > 0 ? diffEpoch : 0)))
 
-            const diffTime = Math.abs(matureDate - current);
+
+            const diffTime = matureDate - current
             const totals = Math.floor(diffTime / 1000)
 
-            const { days, hours, minutes, seconds } = secondsToDDHHMMSS(totals)
+            if (totals > 0) {
+                const { days, hours, minutes, seconds } = secondsToDDHHMMSS(totals)
+                setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+            } else {
+                setCountdown(`0d 0h 0m`)
+            }
 
-            setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+
         },
         1000,
     )
@@ -126,6 +132,8 @@ const StakeStakedSuiToPT = ({
 
     const ptMulfiplier = summary && vault && 1 + ((Number(`${(BigNumber(vault.vault_apy)).dividedBy(BigNumber(10 ** 9))}`)) * (Number(vault.maturity_epoch) - Number(summary.epoch)) / 365)
     const ptAmount = ptMulfiplier ? amount * ptMulfiplier : 0
+
+    const isEnded = summary && vault && Number(vault.maturity_epoch) - Number(summary.epoch) <= 0
 
     return (
         <>
@@ -253,11 +261,18 @@ const StakeStakedSuiToPT = ({
                         name={"APY to lock-in"}
                         value={`${apy}%`}
                     />
-                    <div className="w-full text-xs my-3 font-medium p-2 py-1 text-center rounded border border-blue-400 text-blue-400">
-                        The vault is in private alpha, reach out to the team for grant access
-                    </div>
+                    {!isEnded && (
+                        <div className="w-full text-xs my-3 font-medium p-2 py-1 text-center rounded border border-blue-400 text-blue-400">
+                            The vault is in private alpha, reach out to the team for grant access
+                        </div>
+                    )}
+                    {isEnded && (
+                        <div className="w-full text-xs my-3 font-medium p-2 py-1 text-center rounded border border-yellow-300 text-yellow-300">
+                            The vault has matured
+                        </div>
+                    )}
                     <hr class="h-px my-4 border-0 bg-gray-600" />
-                    <button disabled={disabled} onClick={onMint} className={`py-3 rounded-lg pl-10 pr-10 text-sm font-medium flex flex-row w-full justify-center bg-blue-700 ${disabled && "opacity-60"}`}>
+                    <button disabled={disabled || isEnded} onClick={onMint} className={` ${isEnded && "opacity-80"}  py-3 rounded-lg pl-10 pr-10 text-sm font-medium flex flex-row w-full justify-center bg-blue-700 ${disabled && "opacity-60"}`}>
                         {loading && <Spinner />}
                         Mint
                     </button>
