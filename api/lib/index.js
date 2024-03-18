c58de638188bdd9da373222f2e5c09cc89e8e51c
pisuthd <pisuth.dae@gmail.com>
2024-03-18 19:26:24 +0900

    fetch token prices from Coingecko

--- api/lib/index.js
+++ api/lib/index.js
@@ -1,11 +1,36 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const TOKENS = require("./token.json")
 
 
-class Lib {
-
-    getTest = async () => {
-        return "hello"
-    }
-
+const getTest = async () => {
+    return "hello"
 }
 
-exports.Lib = Lib
+const getTokenPrices = async () => {
+    let output = []
+    for (let token of TOKENS) {
+
+        const { coinId, coinType } = token
+
+        const { data } = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=usd`, {
+            headers: {
+                "x-cg-pro-api-key": process.env.COINGECKO_API
+            }
+        })
+
+        const price = data[coinId]["usd"]
+
+        output.push({
+            coinId,
+            coinType,
+            price
+        })
+    }
+    return output
+}
+
+exports.getTest = getTest
+
+exports.getTokenPrices = getTokenPrices
--- api/test/index.js
+++ api/test/index.js
@@ -1,21 +1,47 @@
 const chai = require('chai')
-const { Lib } = require("../lib")
+const { getTokenPrices } = require("../lib")
 
 const { expect } = chai
 
 describe('#lib', function () {
 
-    let lib 
 
-    before(function () {
-        lib = new Lib()
+    // before(function () {
+    //     lib = new Lib()
+    // })
+
+    it('should get all token prices from CoinGecko success ', async () => {
+
+        const prices = await getTokenPrices() 
+
+        expect(prices.length).to.equal(1)
+        expect(prices[0].price > 0.1).to.true
     })
 
-    it('should get hello success ', async () => {
+    // it('test dates', async () => {
 
-        const output = await lib.getTest() 
+    //     let currentDate = new Date()
 
-        expect(output).to.equal("hello")
-    })
+    
+    //     currentDate.setUTCHours(0)
+    //     currentDate.setUTCMinutes(0)
+    //     currentDate.setUTCSeconds(0)
+    //     currentDate.setUTCMilliseconds(0)
+
+    //     console.log("previous #1 --> ", currentDate.valueOf())
+
+    //     console.log("previous #2 --> ", currentDate.setDate(currentDate.getDate() - 1))
+
+    //     console.log("previous #3 --> ", currentDate.setDate(currentDate.getDate() - 1))
+
+    //     console.log("previous #4 --> ", currentDate.setDate(currentDate.getDate() - 1))
+    //     console.log("previous #5 --> ", currentDate.setDate(currentDate.getDate() - 1))
+
+    //     console.log("previous #6 --> ", currentDate.setDate(currentDate.getDate() - 1))
+    //     console.log("previous #7 --> ", currentDate.setDate(currentDate.getDate() - 1))
+    //     console.log("previous #8 --> ", currentDate.setDate(currentDate.getDate() - 1))
+
+    //     expect(true).to.true
+    // })
 
 })
