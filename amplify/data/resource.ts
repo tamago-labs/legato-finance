3bd23c4b044461e34136cf22c6eec2de38cc6fd6
pisuthd <pisuth.dae@gmail.com>
2025-02-13 19:54:35 +0900

    Add db schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,123 +1,73 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  // User: a.model({
-  //   username: a.string().required(),
-  //   image: a.string(),
-  //   wallets: a.hasMany("Wallet", "userId"),
-  //   markets: a.hasMany('Market', "creatorId"),
-  //   predictions: a.hasMany('Prediction', "userId"),
-  //   reviews: a.hasMany('Review', "userId"),
-  //   credits: a.integer(),
-  //   leaderboardEntry: a.hasOne('LeaderboardEntry', "userId"),
-  //   role: a.enum(["USER", "CREATOR", "ADMIN"])
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // Wallet: a.model({
-  //   userId: a.id().required(),
-  //   user: a.belongsTo('User', "userId"),
-  //   walletAddress: a.string().required(),
-  //   chainId: a.string()
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // Market: a.model({
-  //   creatorId: a.id().required(),
-  //   title: a.string(),
-  //   image: a.string(),
-  //   description: a.string(),
-  //   outcomes: a.string().array(),
-  //   status: a.enum(['ACTIVE', 'CLOSED', 'RESOLVED']),
-  //   resolvedOutcome: a.integer(),
-  //   closingDate: a.datetime(),
-  //   resolutionDate: a.datetime(),
-  //   createdDate: a.datetime(),
-  //   category: a.string(),
-  //   tags: a.string().array(),
-  //   chainId: a.string(),
-  //   round: a.integer(),
-  //   marketType: a.integer(),
-  //   creator: a.belongsTo('User', "creatorId"),
-  //   predictions: a.hasMany('Prediction', "marketId"),
-  //   reviews: a.hasMany('Review', "marketId")
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // Prediction: a.model({
-  //   marketId: a.id().required(),
-  //   userId: a.id().required(),
-  //   user: a.belongsTo('User', "userId"),
-  //   market: a.belongsTo('Market', "marketId"),
-  //   outcome: a.integer(),
-  //   stake: a.integer(),
-  //   currency: a.string(),
-  //   status: a.enum(['OPEN', 'CLOSED', 'RESOLVED', 'CANCELLED']),
-  //   createdDate: a.datetime()
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // LeaderboardEntry: a.model({
-  //   userId: a.id().required(),
-  //   user: a.belongsTo('User', "userId"),
-  //   totalPredictions: a.integer(),
-  //   successfulPredictions: a.integer(),
-  //   totalPnL: a.float(),
-  //   rank: a.integer()
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // Review: a.model({
-  //   marketId: a.id().required(),
-  //   userId: a.id().required(),
-  //   user: a.belongsTo('User', "userId"),
-  //   market: a.belongsTo('Market', "marketId"),
-  //   rating: a.integer(),
-  //   content: a.string(),
-  //   createdDate: a.datetime()
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // TokenPrice: a.model({
-  //   symbol: a.string().required(),
-  //   image: a.string(),
-  //   price: a.float(),
-  //   timestamp: a.datetime(),
-  //   source: a.string(),
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // Resource: a.model({
-  //   name: a.string().required(),
-  //   description: a.string(),
-  //   url: a.string(),
-  //   context: a.string(),
-  //   category: a.string(),
-  //   crawled_data: a.string(),
-  //   last_crawled_at: a.timestamp()
-  // }).authorization((allow) => [allow.publicApiKey()]),
-  // OutcomeSegmentationAI: a.generation({
-  //   aiModel: a.ai.model('Claude 3.5 Sonnet'),
-  //   systemPrompt: 'You are an intelligent assistant that analyzes website content and assists in DeFi market prediction creation by suggesting possible outcomes.',
-  //   inferenceConfiguration: {
-  //     temperature: 1,
-  //     topP: 0.999,
-  //     maxTokens: 4096
-  //   }
-  // })
-  //   .arguments({
-  //     description: a.string()
-  //   })
-  //   .returns(
-  //     a.customType({
-  //       outcomes: a.string().array(), // Suggested outcomes (e.g., A, B, C, D)
-  //     })
-  //   )
-  //   .authorization((allow) => allow.publicApiKey()),
-  // MarketCreationAI: a.generation({
-  //   aiModel: a.ai.model('Claude 3.5 Sonnet'),
-  //   systemPrompt: 'You are an AI assistant that analyzes website content to generate market predictions.',
-  //   inferenceConfiguration: {
-  //     temperature: 1,
-  //     topP: 0.999,
-  //     maxTokens: 4096
-  //   }
-  // })
-  //   .arguments({
-  //     description: a.string()
-  //   })
-  //   .returns(
-  //     a.customType({
-  //       result: a.string()
-  //     })
-  //   )
-  //   .authorization((allow) => allow.publicApiKey()),
+  User: a
+    .model({
+      username: a.string().required(),
+      image: a.url(),
+      markets: a.hasMany('Market', "managerId"),
+      comments: a.hasMany('Comment', "userId"),
+      positions: a.hasMany('Position', "userId"),
+      role: a.enum(["USER", "MANAGER", "ADMIN"])
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+  Market: a
+    .model({
+      managerId: a.id().required(),
+      manager: a.belongsTo('User', "managerId"),
+      onchainId: a.integer(),
+      title: a.string().required(),
+      description: a.string(),
+      chains: a.string().array(),
+      image: a.url(),
+      resource: a.hasOne('Resource', "marketId"),
+      category: a.string(),
+      tags: a.string().array(),
+      currency: a.string()
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+  Comment: a.model({
+    marketId: a.id().required(),
+    userId: a.id().required(),
+    user: a.belongsTo('User', "userId"),
+    market: a.belongsTo('Market', "marketId"),
+    rating: a.integer(),
+    content: a.string()
+  }).authorization((allow) => [allow.publicApiKey()]),
+  Position: a.model({
+    marketId: a.id().required(),
+    market: a.belongsTo('Market', "marketId"),
+    userId: a.id().required(),
+    user: a.belongsTo('User', "userId"),
+    roundId: a.integer(),
+    onchainId: a.integer(),
+    chain: a.string(),
+    predictedOutcome: a.integer(),
+    betAmount: a.integer(),
+    hidden: a.boolean(),
+    status: a.enum(["PENDING", "WIN", "LOSE", "CANCELLED"]),
+    walletAddress: a.string()
+  }).authorization((allow) => [allow.publicApiKey()]),
+  Outcome: a.model({
+    marketId: a.id().required(),
+    market: a.belongsTo('Market', "marketId"),
+    onchainId: a.integer(),
+    roundId: a.integer(),
+    totalBetAmount: a.integer(),
+    title: a.string(),
+    resolutionDate: a.datetime(),
+    status: a.enum(["PENDING", "WIN", "LOSE", "CANCELLED"]),
+    result: a.string()
+  }).authorization((allow) => [allow.publicApiKey()]),
+  Resource: a.model({
+    marketId: a.id().required(),
+    market: a.belongsTo('Market', "marketId"),
+    name: a.string(),
+    url: a.string(),
+    category: a.string(),
+    crawledData: a.string(),
+    lastCrawledAt: a.timestamp()
+  }).authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
