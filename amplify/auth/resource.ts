cd9a224db08b2bc8ebaaf19cad6c5fe7685542e1
pisuthd <pisuth.dae@gmail.com>
2025-02-15 20:35:50 +0900

    Fix build error

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -16,7 +16,7 @@ export const auth = defineAuth({
         'http://localhost:3000/auth/profile',
         'http://legato.finance/auth/profile',
       ],
-      logoutUrls: ['http://localhost:3000/, http://legato.finance'],
+      logoutUrls: ['http://localhost:3000/', 'http://legato.finance'],
     }
   },
 });
