608052e3ca4c48883f50850552563f9247edb2a5
pisuthd <pisuth.dae@gmail.com>
2025-02-18 19:25:35 +0900

    Add Aptos SDK to the handler

--- amplify/functions/faucet/handler.ts
+++ amplify/functions/faucet/handler.ts
@@ -1,9 +1,28 @@
 import type { Handler } from 'aws-lambda';
 import type { Schema } from "../../data/resource"
+import {
+  Account,
+  Aptos,
+  AptosConfig,
+  Network,
+  Ed25519PrivateKey
+} from "@aptos-labs/ts-sdk";
+import { secret } from '@aws-amplify/backend';
+
 
 export const handler: Schema["Faucet"]["functionHandler"] = async (event) => {
-    // arguments typed from `.arguments()`
-    const { name } = event.arguments
-    // return typed from `.returns()`
-    return `Hello, ${name}!`
-  }
+  const { name } = event.arguments
+
+  const config = new AptosConfig({ network: Network.TESTNET });
+  const aptos = new Aptos(config);
+
+  const privateKey = new Ed25519PrivateKey(
+    `${secret('APTOS_MANAGED_KEY')}`
+  );
+
+  const account = Account.fromPrivateKey({
+    privateKey
+  })
+
+  return `Hello, ${account.accountAddress}!`
+}
