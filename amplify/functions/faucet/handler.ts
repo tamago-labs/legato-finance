c8435d4fac6f6020332b77323f43429a7a2814dd
pisuthd <pisuth.dae@gmail.com>
2025-02-18 20:01:20 +0900

    Fix env

--- amplify/functions/faucet/handler.ts
+++ amplify/functions/faucet/handler.ts
@@ -7,18 +7,26 @@ import {
   Network,
   Ed25519PrivateKey
 } from "@aptos-labs/ts-sdk";
-import { secret } from '@aws-amplify/backend';
-
+// import { env } from '$amplify/env/Faucet';
 
 export const handler: Schema["Faucet"]["functionHandler"] = async (event) => {
   const { name } = event.arguments
 
+  console.log("hello1")
+
   const config = new AptosConfig({ network: Network.TESTNET });
+
+  console.log("hello2")
+
   const aptos = new Aptos(config);
 
+  console.log("hello3")
+
   const privateKey = new Ed25519PrivateKey(
-    `${secret('APTOS_MANAGED_KEY')}`
+    `${process.env.APTOS_MANAGED_KEY}`
   );
+
+  console.log("hello4")
 
   const account = Account.fromPrivateKey({
     privateKey
--- amplify/functions/faucet/resource.ts
+++ amplify/functions/faucet/resource.ts
@@ -1,10 +1,14 @@
 
 
 import { defineFunction } from '@aws-amplify/backend';
+import { secret } from '@aws-amplify/backend';
 
 export const faucet = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'faucet',
   // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './handler.ts'
+  entry: './handler.ts',
+  environment: {
+    APTOS_MANAGED_KEY: secret('APTOS_MANAGED_KEY')
+  }
 });
