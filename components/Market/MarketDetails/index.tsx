f29f9a7e948b22994acf8abc10e7605fd0e5335a
pisuthd <pisuth.dae@gmail.com>
2025-02-18 11:27:15 +0900

    Update DB schema to support a new AI-agent

--- components/Market/MarketDetails/index.tsx
+++ components/Market/MarketDetails/index.tsx
@@ -8,6 +8,10 @@ const MarketDetails = () => {
     return (
         <>
             <div className=" grid grid-cols-1 sm:grid-cols-5 gap-3">
+            <div className="col-span-5 mb-2 bg-secondary/10 border-l-4 border-secondary text-secondary p-4 rounded-lg">
+ 
+  <p>AI-Agent for this market is being redeployed. We will be back in a few days.</p>
+</div>
                 <div className="col-span-3 flex flex-col ">
                     <Overview />
                     <WalletPanel />
