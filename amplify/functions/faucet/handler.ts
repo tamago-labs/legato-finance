54d054942d7a30832dcb63f67a5693b758637e80
pisuthd <pisuth.dae@gmail.com>
2025-02-19 10:16:04 +0900

    complete backend faucet

--- amplify/functions/faucet/handler.ts
+++ amplify/functions/faucet/handler.ts
@@ -7,30 +7,39 @@ import {
   Network,
   Ed25519PrivateKey
 } from "@aptos-labs/ts-sdk";
-// import { env } from '$amplify/env/Faucet';
 
 export const handler: Schema["Faucet"]["functionHandler"] = async (event) => {
   const { name } = event.arguments
 
-  console.log("hello1")
-
   const config = new AptosConfig({ network: Network.TESTNET });
 
-  console.log("hello2")
-
   const aptos = new Aptos(config);
-
-  console.log("hello3")
 
   const privateKey = new Ed25519PrivateKey(
     `${process.env.APTOS_MANAGED_KEY}`
   );
 
-  console.log("hello4")
-
   const account = Account.fromPrivateKey({
     privateKey
   })
 
-  return `Hello, ${account.accountAddress}!`
+  const transaction = await aptos.transaction.build.simple({
+    sender: account.accountAddress,
+    data: {
+      function: `0xab3922ccb1794928abed8f5a5e8d9dac72fed24f88077e46593bed47dcdb7775::mock_usdc_fa::mint`,
+      functionArguments: [name, 10000000],
+    },
+  });
+
+  const senderAuthenticator = aptos.transaction.sign({
+    signer: account,
+    transaction,
+  });
+
+  const submittedTransaction = await aptos.transaction.submit.simple({
+    transaction,
+    senderAuthenticator,
+  });
+
+  return `${submittedTransaction.hash}`
 }
